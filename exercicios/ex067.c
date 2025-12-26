151f75d4895f278758570c48d768c057d28099fc
Fabricio Paiva <cdpaiva24530@gmail.com>
2025-12-26 15:45:17 -0300

    Remove compiled binaries and fix .gitignore

--- exercicios/ex067.c
+++
@@ -1,9 +0,0 @@
-/*funcao que retorne um numero inteiro sempre na forma negativa*/
-
-int negativo(int n) {
-    if (n < 0) {
-        return n;
-    } else {
-        return -n ;
-    }
-}
--- exercicios/ex069.c
+++
@@ -1,5 +0,0 @@
-/*funcao que devolve o cubo de x, sendo x inteiro*/
-
-long long cubo(int x) {
-    return (x * x * x);
-}
